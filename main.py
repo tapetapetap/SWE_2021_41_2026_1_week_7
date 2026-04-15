60e93e168c3b1631e5f8f7d5f54e60e1bfb5c5f3
tapetapetap <jiwon.seo@g.skku.edu>
2026-04-15 19:13:55 +0900

    initial version of 7week assignment

---
+++ main.py
@@ -0,0 +1,14 @@
+from typing import List
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+    num_map = {}
+    
+    for i, num in enumerate(nums):
+        complement = target - num
+        
+        if complement in num_map:
+            return [num_map[complement], i]
+        
+        num_map[num] = i
+        
+    return []
